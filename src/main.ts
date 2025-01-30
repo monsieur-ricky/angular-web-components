a012a9771ec7bfb32b00bc6de6c5c83f5dcde510
Ricky <ricky2k7@gmail.com>
2025-01-30 22:49:29 +0100

    Update project to convert the component into a web component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  bootstrapApplication,
+  createApplication
+} from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+createApplication(appConfig)
+  .then((app) => {
+    const appComponent = createCustomElement(AppComponent, {
+      injector: app.injector
+    });
+    customElements.define('awc-counter', appComponent);
+  })
   .catch((err) => console.error(err));
