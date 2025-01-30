f562a4531e396ffbdf0e80122aa810b6fa3183e1
Ricky <ricky2k7@gmail.com>
2025-01-30 22:42:11 +0100

    Create basic counter demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, input, linkedSignal } from '@angular/core';
 
 @Component({
   selector: 'awc-root',
   imports: [],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
-  styles: []
+  template: ` <div>
+    <h1>{{ title() }}</h1>
+    <h2 [class.negative]="count() < 0">{{ count() }}</h2>
+    <button (click)="count.set(count() + 1)">Count Up</button>
+    <button (click)="count.set(count() - 1)">Count Down</button>
+    <button (click)="onReset()">Reset</button>
+  </div>`,
+  styles: `
+    :host {
+      --red-color: #af0000;
+      --green-color: #05860c;
+
+      text-align: center;
+
+      h1, h2 {
+        font-family: Arial, sans-serif;
+      }
+
+      h2 {
+        color: var(--green-color);
+        font-size: 300%;
+
+        &.negative {
+          color: var(--red-color);
+        }
+      }
+
+      button {
+        background-color: #555555;
+        border: none;
+        border-radius: 5px;
+        color: white;
+        cursor: pointer;
+        font-size: 100%;
+        margin: 5px;
+        padding: 5px 10px;
+
+        &:first-of-type {
+          background-color: var(--green-color);
+        }
+
+        &:nth-of-type(2) {
+          background-color: var(--red-color);
+        }
+      }
+    }    
+  `
 })
 export class AppComponent {
-  title = 'Angular Web Components';
+  title = input('Web Components with Angular');
+  value = input(0);
+
+  count = linkedSignal(() => this.value());
+
+  onReset(): void {
+    this.count.set(this.value());
+  }
 }
